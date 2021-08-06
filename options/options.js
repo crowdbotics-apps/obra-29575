738284dbce4c4300db76817cd51c6c2f36ee4b34
Crowdbotics <team@crowdbotics.com>
2021-08-06 18:23:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "obra_29575Identifier",
+  url: "https://obra_29575Identifier.botics.co",
+  api: "https://obra_29575Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
